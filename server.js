586ea3384c59420635575ee5973bbb085d76f11b
daniel <danielsrbu@gmail.com>
2024-12-02 22:06:14 -0600

    act

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
+// server.js
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const app = express();
 const port = 3000;
 
-// Ruta para cargar los datos del archivo JSON
-app.get('/nombres', (req, res) => {
-  // Lee el archivo datos.json
-  fs.readFile(path.join(__dirname, 'datos.json'), 'utf8', (err, data) => {
-    if (err) {
-      return res.status(500).send('Error al cargar los datos.');
-    }
+app.use(express.static('public')); // Servir archivos estáticos
 
-    // Envía los datos JSON al cliente
-    res.json(JSON.parse(data));
-  });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/src/index.html');
 });
 
-// Inicia el servidor
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor en http://localhost:${port}`);
 });
